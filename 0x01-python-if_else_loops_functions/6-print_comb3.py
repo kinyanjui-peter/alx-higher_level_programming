cd19ba85fbcd127b4f0cfbe316c78550ac3317bd
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-07 15:49:44 -0800

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,4 @@ for i in range(0, 90):
         for x in range(i + 1, 10):
             print("{}{}, ".format(i, x), end="")
     if i == 89:
-        print("{}\n".format(i, x))
+        print("{}".format(i, x))
