d26fd5a7a6e5dddc932e7bf2a7be6aa0e29d5ccc
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-08 14:11:34 -0800

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,4 +5,5 @@ def upper(str):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             nxt = chr(ord(str[i]) - 32)
             print("{}".format(nxt), end="")
-            print("")
+        else:
+            print("{}".format(str[i]), end="")
