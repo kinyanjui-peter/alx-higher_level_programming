5063b17e16ee4ba13b8fcae6199fe30d12af23fc
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-08 13:12:18 -0800

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def upper(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            nxt = chr(ord(str[i]) - 32)
+            print("{}".format(nxt), end="")
+        else:
+            print("{}".format(str[i]), end="")
+    print("")
