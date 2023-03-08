a34795b5333c428ff6fb424a92ad7bd107d612ed
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-08 12:01:38 -0800

    task7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def islower(c):
+    if c >= 'a' and c <= 'z':
+        return True
+    else:
+        return False
+
+
+name = input("Enter a character: ")
+print(islower(name))
