9c4a3209d718e08a9d1ef4d1c0747919c36efeed
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-09 12:50:58 -0800

    task0b

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# function that adds two numbers
-
 def add(a, b):
-    a = 1
-    b = 2
-    return a + b
-if __name__ =="__main__":
-    add(1, 2)     
+    return (a + b)
+if __name__ == "__main__":
+    add()
