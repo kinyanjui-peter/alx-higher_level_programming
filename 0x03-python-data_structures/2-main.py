7be76f0c74d778aae76f118675367683f9258b64
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-13 14:21:06 -0700

    task2

--- 0x03-python-data_structures/2-main.py
+++ 0x03-python-data_structures/2-main.py
@@ -2,8 +2,8 @@
 replace_in_list = __import__('2-replace_in_list').replace_in_list
 
 my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
+idx = int(input("enter the index: "))
+new_element = int(input("enter the new element: "))
 new_list = replace_in_list(my_list, idx, new_element)
 
 print(new_list)
