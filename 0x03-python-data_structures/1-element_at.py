4c2b5e72a38b20b619eaab89329eca6a951b62de
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-13 10:57:51 -0700

    task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -13,7 +13,6 @@ def element_at(my_list, idx):
     if idx < 0 or idx > (len(my_list) - 1):
         return None
     return (my_list[idx])
-print("Element at index {} is {}".format(idx, my_list[idx])) 
 
 #idx = int(input("enter the index: "))
 #my_list = [1, 2, 3, 4, 5]
