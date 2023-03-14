e242e1cdd3260ceae3929450ad5825325cc4d00c
kinyanjui <01peterkinyanjui@gmail.com>
2023-03-13 18:23:51 -0700

    task2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-#replace_in_list - Replaces content of a list
+# replace_in_list - Replaces content of a list
 
 def replace_in_list(my_list, idx, element):
-    
     if idx >= 0 and idx < len(my_list):
-       my_list[idx] = element
+        my_list[idx] = element
     return (my_list)
