d6fb5c24248790b018dbafa3ecbbab444cf4d150
peter <01peterkinyanjui@gmail.com>
2023-08-08 12:00:14 -0700

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for x in range(100):
-    if x == 99:
-        print("{:02d}". format(x), end="\n")
-        break
-    print("{:02d}". format(x), end=", ")
+for x in range(0, 99):
+#    if x == 99:
+#        print("{:02d}". format(x), end="\n")
+#        break
+#    print("{:02d}". format(x), end=", ")
+    print("{:02d}".format(x), end=', ')
+print("{:02d}".format(x + 1))
