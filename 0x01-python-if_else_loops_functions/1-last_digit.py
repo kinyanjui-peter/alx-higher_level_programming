3d636705ef0260d49196ec41ca7b25b62d413e1c
peter <01peterkinyanjui@gmail.com>
2023-08-08 07:04:12 -0700

    task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+mod = abs(number % 10)
+if mod > 5:
+    print(f"Last digit of {number} is {mod} and is greater than 5")
+elif mod == 0:
+    print(f"Last digit of {number} is {mod} and is 0")
+elif (6 > mod) and (mod != 0):
+    print(f"Last digit of {number} is {mod} and is less than 6 and not 0")
+print("\n")
