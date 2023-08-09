6e7bcf95043d3a335c22e9c70a50920436679ab7
peter <01peterkinyanjui@gmail.com>
2023-08-09 00:05:29 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-for num in range(0, 10):
+for num in range(0, 8):
     for x in range(num + 1, 10):
-        if num != x:
-            if (num == 8) and (x == 9):
-                print("{}{}". format(num, x), end="\n")
-                break
-            print("{}{}". format(num, x), end=", ")
-    num += 1
+#        if num != x:
+#            if (num == 8) and (x == 9):
+        print("{:d}{:d}". format(num, x), end=", ")
+#                break
+print("{:d}{:d}". format(num + 1, x))
+#    num += 1
+
