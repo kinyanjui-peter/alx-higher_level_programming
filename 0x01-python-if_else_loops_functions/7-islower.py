77136e1c2fb348e36ae82dc698530b8fa9c612e8
peter <01peterkinyanjui@gmail.com>
2023-08-09 13:05:57 -0700

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if (ord(c) >= (97) and ord(c) <= (122):
+    if ord(c) >= 97 and ord(c) <= 122:
        # print("{:s} is lower". format(c))
        return true
     else:
