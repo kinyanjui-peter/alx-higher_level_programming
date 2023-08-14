a0be20861fad2fe58935e4b90d0099a1de31719e
peter <01peterkinyanjui@gmail.com>
2023-08-13 23:30:58 -0700

    task 2

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx > 0:
+        new_list = my_list.copy[:]
+        new_list[idx] = element
+        print("{}".format(new_list)
+    else:
+                return mylist(idx)
+    if idx > len(mylist):
+        return my_list
