6417e279ff018ba7c3873a73bc2de7b6e76bda8b
peter <01peterkinyanjui@gmail.com>
2023-08-14 19:04:10 -0700

    task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx > 0:
-        new_list = my_list.copy[:]
-        new_list[idx] = element
-        print("{}".format(new_list)
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
     else:
-                return mylist(idx)
-    if idx > len(mylist):
-        return my_list
+        return my_list.pop(idx)
