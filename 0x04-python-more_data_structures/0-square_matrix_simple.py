a0a9e63f9436613f094a25411373bf71d0782785
peter <01peterkinyanjui@gmail.com>
2023-08-20 14:08:31 -0700

    task1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,4 @@
 # matrix - the name 
 def square_matrix_simple(matrix=[]):
     squared_matrix = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
-    return squared_matrix
-
-
-  
+    return squared_matrix 
