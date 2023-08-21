c0722015608870e100f8600df2944abd31e6f0b9
peter <01peterkinyanjui@gmail.com>
2023-08-20 21:07:48 -0700

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary):
     sorted_dict = sorted(a_dictionary.keys())
     for key in sorted_dict:
         value = a_dictionary[key]
-        print(f"{key}:{value}")
+        print(f"{key}: {value}")
