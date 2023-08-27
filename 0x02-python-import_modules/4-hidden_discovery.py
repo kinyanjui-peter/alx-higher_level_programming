f4114ee88385ed8a00e0f869e4887025ec5087b0
peter <01peterkinyanjui@gmail.com>
2023-08-27 13:43:33 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == '__main__':
     import hidden_4
     x = dir(hidden_4)
-        for counter in range(0, len(x)):
-            if x[counter][0:2] != "__":
-                print("{}".format(x[counter]))
+    for counter in range(0, len(x)):
+        if x[counter][0:2] != "__":
+            print("{}".format(x[counter]))
