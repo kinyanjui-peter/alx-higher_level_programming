4073f009e7c8720eac516c5d87f01562dd208cde
peter <01peterkinyanjui@gmail.com>
2023-08-28 13:44:28 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""safe_print_list - funtion name
+my_list = array of list
+x: number in list"""
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    for index in range(x):
+        try:
+            print("{}".format(my_list[index]), end="")
+            count += 1
+        except IndexError:
+            break
+    print("")
+    return count
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,3 @@
+This directory contains study solutions to errors and exceptions task.
+Done by:
+MelDev_Tech
