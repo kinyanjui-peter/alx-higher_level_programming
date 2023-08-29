863e883b71dde22255ce36f0e47fc7ee73d73086
peter <01peterkinyanjui@gmail.com>
2023-08-28 17:57:29 -0700

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+
+
+"""
+    Divides two lists element by element.
+    my_list_1 (list): The first list.
+    my_list_2 (list): The second list.
+    list_length (int): The number of elements to divide.
+    Returns:A new list of length list_length.
+    """
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+
+    for counter in range(0, list_length):  # Changed 'i' to 'counter'
+        try:
+            res = my_list_1[counter] / my_list_2[counter]  # Changed 'div' to 'res'
+        except TypeError:
+            print("wrong type")
+            res = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            res = 0
+        except IndexError:
+            print("out of range")
+            res = 0
+        finally:
+            new_list.append(res)
+    return new_list
