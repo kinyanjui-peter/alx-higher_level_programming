8945ffda43228b4a36ccf42a43c556604cbc0229
peter <01peterkinyanjui@gmail.com>
2023-09-03 19:17:24 -0700

    task1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,21 +10,6 @@ def add_integer(a, b=98):
     args:
         a(int, float) the first number
         b(int, float) the second number
-
-    >>> add_integer(4, 10)
-    14
-    >>> add_integer(4, 'School')
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be float or integers'
-    >>> add_integer('School', 10)
-    Traceback (most recent call last):
-        ...
-    TypeError: must be float or integers'
-    >>> add_integer(-4, 10)
-    6
-    >>> add_integer(14.5, -10)
-    4
         """
     if not isinstance(a,(int, float)):
         raise TypeError('a imust be an integer')
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
     #check if matrix is a list and each element is an int or float
-    if not all(isinstance(row, list) or not all(isinstance(row, (int, float) for row in matrix:
+    if not all(isinstance(row, list) and all(isinstance(elem, 
+        (int, float))for elem in row) for row in matrix):
         raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
     
     #check if all rows are of the same size
-    row_length = len(row) for row in matrix
-    if not(len(row) < row_length):
+    row_length = set(len(row) for row in matrix)
+    if (len(row_length) > 1):
         raise TypeError('Each row of the matrix must have the same size')
 
     #check if div is an int or float
@@ -23,6 +24,4 @@ def matrix_divided(matrix, div):
         row_2 = [(round((x % div) == 0), 2) for x in row]
         matrix_2.append(row_2)
 
-    return new matrix
-
-
+    return matrix_2
