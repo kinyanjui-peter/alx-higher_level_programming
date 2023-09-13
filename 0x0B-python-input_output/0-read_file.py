b9231330ce5ad9ac2cbaa2d1bd3ba22dc5d3980d
peter <01peterkinyanjui@gmail.com>
2023-09-12 21:53:36 -0700

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    reads a text file (UTF8) and prints it to stdout
+    reads a text file (UTF-8) and prints it to stdout
     args:
         filename: hold the name of the file to be read
     """
@@ -13,8 +13,10 @@ def read_file(filename=""):
     try:
         with open(filename, encoding='UTF8', mode='r') as file:
             file.seek(0)
-            for line in filename:
+            line = file.read()
                 print(line, end="")
+                """
+                file not found error"""
     except FileNotFound:
         print(f"File *'{}' not found.")
     except exception as e:
