2da5bb664a86401dfc1a682183570035e0bb326e
peter <01peterkinyanjui@gmail.com>
2023-09-13 01:39:37 -0700

    task3

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-    json file
-    """
+json file
+"""
 import json
-    """
-        function that returns an object (Python data structure)
-         represented by a JSON string
-        """
-    def from_json_string(my_str):
-        return json.loads(my_str)
+"""
+function that returns an object (Python data structure)
+represented by a JSON string
+"""
+def from_json_string(my_str):
+    return json.loads(my_str)
