f7198825f5162dec1c7e349cdfff574635ab3edb
peter <01peterkinyanjui@gmail.com>
2023-09-18 01:26:00 -0700

    task2

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""import unittest"""
+import unittest
+""" import the base module to be tested"""
+from models.rectangle import rectangle(Base)
+class width(self):
+    """test if value of width is NULL"""
+    def test_with_null(self):
+    """test if value of width is 0 """
+    """test if value of width negative """
+    """test if value of width  positive"""
+    """test if value of width None"""
+    """test if value of width  not int or float"""
+class height(self):
+    """test if value of height is NULL"""
+    """test if value of height is 0 """
+    """test if value of height negative """
+    """test if value of height  positive"""
+    """test if value of height None"""
+    """test if value of height  not int or float"""
+class x(self):
+    """test if value of x is NULL"""
+    """test if value of x is 0 """
+    """test if value of x negative """
+    """test if value of x  positive"""
+    """test if value of x None"""
+    """test if value of x  not int or float"""
+class y(self):
+    """test if value of y is NULL"""
+    """test if value of y is 0 """
+    """test if value of y negative """
+    """test if value of y  positive"""
+    """test if value of y None"""
+    """test if value of y  not int or float"""
