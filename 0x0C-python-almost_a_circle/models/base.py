a3940ede710b72be6f616841553f0724bf58ed1b
peter <01peterkinyanjui@gmail.com>
2023-09-19 00:14:09 -0700

    task14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""import JSON"""
+import json
 """creation of class base"""
 class Base:
     """private class attribute"""
     __nb_objects = 0
+
     """init method defination"""
     def __init__(self, id=None):
         if id is not None:
@@ -10,3 +13,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    """JSON data representation."""
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            if len(list_dictionaries) == 0:
+                return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
