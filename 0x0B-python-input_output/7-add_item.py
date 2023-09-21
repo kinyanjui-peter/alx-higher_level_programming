4845b8b4dad2afbfa4605df8b4c25cf1a8566578
peter <01peterkinyanjui@gmail.com>
2023-09-21 00:40:47 -0700

    task3

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
+# 9-add_item.py
+"""Add all arguments to a Python list and save them to a file."""
 import sys
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-# Check if "add_item.json" exists and load its content if it exists.
-try:
-    existing_data = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    existing_data = []
-
-# Add command-line arguments to the existing data list.
-arguments = sys.argv[1:]  # Exclude the script name itself.
-existing_data.extend(arguments)
-
-# Save the updated list to "add_item.json".
-save_to_json_file(existing_data, "add_item.json")
-
-#Arguments added and saved successfully to add_item.json.
-
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
