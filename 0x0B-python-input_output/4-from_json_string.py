deadf6814b6515d2de638bdb8ff9e3b824a016f9
peter <01peterkinyanjui@gmail.com>
2023-09-21 00:02:24 -0700

    task3

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -10,4 +10,6 @@ represented by a JSON string
 
 
 def from_json_string(my_str):
+    """returns an object (Python data structure)
+    represented by a JSON string"""
     return json.loads(my_str)
