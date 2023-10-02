fe4ae5566dd33c4b59cac77cae48403450c1dc29
peter <01peterkinyanjui@gmail.com>
2023-10-02 06:34:54 -0700

    task12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n):
+    """
+    Function that prints Pascal's Triangle up to row n.
+    """
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+        
+        for j in range(1, i):
+            new_value = prev_row[j - 1] + prev_row[j]
+            new_row.append(new_value)
+        
+        new_row.append(1)
+        triangle.append(new_row)
+    
+    # Print the Pascal's Triangle
+    for row in triangle:
+        print(" ".join(map(str, row)).center(n * 3))
