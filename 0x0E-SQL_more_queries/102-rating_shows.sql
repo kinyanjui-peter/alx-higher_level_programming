ab75ba6217d8e4966ad503a69764c9c56f5fa9b3
peter <01peterkinyanjui@gmail.com>
2023-10-18 18:35:33 -0700

    task 18

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,12 @@
+-- Import the database hbtn_0d_tvshows_rate dump to your MySQL server: download
+
+-- A script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in descending order by the rating
+-- You can use only one SELECT statement
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
