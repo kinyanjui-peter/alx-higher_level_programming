3afe7b8dda49bcc77dbbe7c594995bae91e7ed2d
peter <01peterkinyanjui@gmail.com>
2023-10-27 03:22:11 -0700

    task10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -6,5 +6,4 @@
 --The database name will be passed as an argument of the mysql command
 
 
-SELECT score, name
-FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
