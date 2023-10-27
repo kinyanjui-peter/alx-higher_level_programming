d1312ae603aa197d96f2f4641de8dfaa09204935
peter <01peterkinyanjui@gmail.com>
2023-10-27 03:06:37 -0700

    task4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,11 @@
+-- A cscript that creates a table called first_table in 
+--	the current DB in my MySQL server
+-- If the table first_table already exists, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
+-- attributes include id and name
+
+
+CREATE TABLE IF NOT EXISTS first_table(
+    id INT,
+    name VARCHAR(256)
+);
