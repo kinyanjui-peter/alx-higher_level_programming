26d855a781950f9576349e13dda58a4ddfe5f7b7
peter <01peterkinyanjui@gmail.com>
2023-11-15 18:55:58 -0800

    task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-function Message(args)
+const { argv } = require('process');
 {
-	if (args.length) === 0
+	if (argv.length === 2)
 	{
 		console.log('No argument');
 	}
-	else if (args.length === 1)
+	else if (argv.length === 3)
 	{
 		console.log('Argument found');
 	}
