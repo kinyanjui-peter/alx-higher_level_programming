d8ac6bb347dcde32eb6506ff6a2864fdd81cbf41
peter <01peterkinyanjui@gmail.com>
2023-11-18 03:06:56 -0800

    task4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
 
-const { argv } = require(`process`);
+const { argv } = require('process');
 
 const first = argv[2];
 const second = argv[3];
 
-if ((argv[2] == null) && (argv[3] ==null)) {
-  console.log(`undefined is undefined`);
-//  const first = argv[2];
-//  const second = argv[3];
-} else if ((argv[2] == null) && (argv[3] != null)) {
-    console.log(`undefined is ${second}`);
+if ((argv[2] == null) && (argv[3] == null)) {
+  console.log('undefined is undefined');
 }
-  else if ((argv[2] != null) && (argv[3] == null)) {
-    console.log(`${first}  is undefined`);
+else if ((argv[2] == null) && (argv[3] != null)) {
+  console.log(`undefined is ${second}`);
 }
-  else if ((argv[2] != null) && (argv[3] != null)) {
-    console.log(`${first} is ${second}`);
+else if ((argv[2] != null) && (argv[3] == null)) {
+  console.log(`${first} is undefined`);
 }
-
+else if ((argv[2] != null) && (argv[3] != null)) {
+  console.log(`${first} is ${second}`);
+}
