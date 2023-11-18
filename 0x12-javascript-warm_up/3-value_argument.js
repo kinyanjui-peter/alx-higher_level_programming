619732314b6af446c2896da08f0470c2288fb67c
peter <01peterkinyanjui@gmail.com>
2023-11-18 01:53:12 -0800

    task3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,6 +4,6 @@ const { argv } = require('process');
 if (argv[2] == null) {
   console.log('No argument');
 } else {
-const firstElem = argv[2];
-console.log(firstElem);
+  const firstElem = argv[2];
+  console.log(firstElem);
 }
