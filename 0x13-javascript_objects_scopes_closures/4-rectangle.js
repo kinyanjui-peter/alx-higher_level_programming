b01f72b0a245aa6c2ebe1609897cd658a2d99485
peter <01peterkinyanjui@gmail.com>
2023-11-17 21:29:34 -0800

    task4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+/* Write a class Rectangle that defines a rectangle:
+ * You must use the class notation for defining your class
+ * The constructor must take 2 arguments: w and h
+ * Initialize the instance attribute width with the value of w
+ * Initialize the instance attribute height with the value of h
+ * If w or h is equal to 0 or not a positive integer, create an empty object
+ * Create an instance method called print() that prints the rectangle using
+ *               the character X */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+    // will print an empty undefined on the console.
+    } else {
+      this.width = w;
+      this.height = h;
+      console.clear();
+      
+    }
+  }
+
+  print () { 
+    for (let c = 1; c <= this.height; c++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate () {
+    for (let c = 1; c <= this.width; c++) {
+      console.log('X'.repeat(this.height));
+    }
+
+  }
+  double () {
+    for (let c = 1; c <= (this.width * 2); c++) {
+      console.log('X'.repeat(this.height * 2));
+    }
+  }
+}
+module.exports = Rectangle;
