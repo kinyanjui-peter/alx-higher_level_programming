c0da0c23874349202bcac8b3fa6974c13a622ff2
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-12-11 23:06:57 +0300

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that get URL, sends a request to that URL, and displays the size of the body of the response
-#respoce_size=$(curl -I "$1" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r\n')
-response_size=$(curl -s "$url" | wc -c)
+respoce_size=$(curl  "$1" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r\n')
