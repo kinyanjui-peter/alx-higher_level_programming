d01c8ad6d656f814999058dc57431bd16ff80eef
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-12-26 23:13:24 +0300

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,14 @@ You must use a with statement
 """
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as req:
-    content = req.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode('utf-8'))
+url = "https://alx-intranet.hbtn.io/status"
+
+def read_url(url):
+    with urllib.request.urlopen(url) as req:
+        content = req.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf-8'))
+if __name__ == "__main__":
+    read_url(url)
