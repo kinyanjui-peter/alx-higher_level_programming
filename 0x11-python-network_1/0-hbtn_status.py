82468195a4942ae559e8fe11f2f6ac39cf17a780
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-12-26 22:54:58 +0300

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+You must use the package urllib
+You are not allowed to import any packages other than urllib
+The body of the response must be displayed like the following example (tabulation before -)
+You must use a with statement
+"""
+import urllib.request
+
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as req:
+    content = req.read()
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", content.decode('utf-8'))
