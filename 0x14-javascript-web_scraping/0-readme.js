beee13853df0733ce8962f7cee98d2e2617180c1
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-04 05:25:34 +0300

    task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
 /**
- *  A script that reads and prints the content of a file 
+ *  A script that reads and prints the content of a file
  *  The first argument is the file path
  *  The content of the file must be read in utf-8
  *  If an error occurred during the reading,
  *  print the error object
  */
- 
 const fs = require('fs');
 
 // file to read
-read_file =  'cisfun';
+const readFile = 'cisfun';
 
 // Read cisfun file
-fs.readFile(read_file, 'utf-8', (err, data) => {
-    if (err)
-    {
-        console.error(`Cannot read file: ${error.message}`);
-    }   
-    else 
-    {
-        console.log(`The file ${read_file} contain:\n${data}`);
-    }
+fs.readFile(readFile, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(`Cannot read file: ${err.message}`);
+  } else {
+    console.log(`The file ${readFile} contain:\n${data}`);
+  }
 });
