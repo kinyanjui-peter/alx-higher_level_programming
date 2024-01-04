307dd517f6a109736f9d3e6e3f494e6c630f1943
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-04 17:55:54 +0300

    task2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /**
- * A script that prints the title of a Star Wars movie where the episode 
+ * A script that prints the title of a Star Wars movie where the episode
  * number matches a given integer.
  * The first argument is the movie ID
- * You must use the Star wars API with the endpoint 
+ * You must use the Star wars API with the endpoint
  * https://swapi-api.alx-tools.com/api/films/:id
  * You must use the module request
  */
@@ -20,13 +20,13 @@ const urlEndpoint = `https://swapi-api.alx-tools.com/api/films/${numMovie}`;
 
 request.get(urlEndpoint, (error, response, body) => {
   if (error) {
-    console.error(`Could not connect to the movie API`);
+    console.error('Could not connect to the movie API');
   } else {
     // Parse the response body to JSON
     const movieInfo = JSON.parse(body);
 
     // Check if the episode number matches the given integer
-    if (movieInfo.episode_id == numMovie) {
+    if (movieInfo.episode_id === numMovie) {
       console.log(`${movieInfo.title}`);
     } else {
       console.log(`Movie with ID ${numMovie} not found`);
