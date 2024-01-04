1be8d652227deea6bde3c70d0781425ce67a0d64
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-04 09:46:38 +0300

    task1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/**
+ * a script that writes a string to a file.
+ * The first argument is the file path
+ * The second argument is the string to write
+ * The content of the file must be written in utf-8
+ * If an error occurred during while writing, print the error object
+ */
+
+const fs = require('fs');
+
+// check if process has a argument
+if (process.argv.length !== 4) {
+  console.error('No argument found');
+  process.exit(1);
+}
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('file successfully written');
+  }
+});
