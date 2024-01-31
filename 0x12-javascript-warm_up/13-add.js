dc915a2fac8e764f4151ab6706b8d912e953f9fd
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-31 18:38:35 +0300

    exports a function

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * function that returns the addition of 2 integers.
+ * function must be visible from outside
+ * name of the function must be add
+ * You are not allowed to use var
+*/
+exports.add = function (a, b) {
+    a = parseInt(a);
+    b = parseInt(b);
+
+    return (a + b);
+}
