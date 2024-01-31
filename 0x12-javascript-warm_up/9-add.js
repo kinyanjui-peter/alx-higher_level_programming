427b393a1965d44f5d2b74afefe77f1121d57158
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-31 13:39:39 +0300

    adding 2 args

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
 /* script that prints the addition of 2 integers
- * first argument is the first integer 
+ * first argument is the first integer
  * second argument is the second integer */
 
 const process = require('process');
 const a = process.argv[2];
 const b = process.argv[3];
 
-function add(a, b) {
-    const numA = parseInt(a);
-    const numB = parseInt(b);
+function add (a, b) {
+  const numA = parseInt(a);
+  const numB = parseInt(b);
 
-    if (isNaN(numA) || isNaN(numB)) {
-        console.log("NaN");
-    } else {
-        console.log(numA + numB);
-    }
+  if (isNaN(numA) || isNaN(numB)) {
+    console.log('NaN');
+  } else {
+    console.log(numA + numB);
+  }
 }
-
 add(a, b);
