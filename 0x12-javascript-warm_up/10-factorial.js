cd06ab5976da6f86f8ad5796caf1769df71dbdce
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-31 14:36:32 +0300

    factorial of a number

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -7,7 +7,7 @@ const process = require('process');
 const arg = parseInt(process.argv[2]);
 
 function factorial (n) {
-  if (n === 0 || n === 1) {
+  if (isNaN(n) || n === 0 || n === 1) {
     return 1;
   } else if (n > 1) {
     return n * factorial(n - 1);
