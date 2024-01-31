8052104cb5c81df2b100aadae7004693f639594c
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-31 14:21:01 +0300

    factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/* script that computes and prints a factorial
+ * first argument is integer
+ */
+
+const process = require('process');
+const arg = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else if (n > 1) {
+    return n * factorial(n - 1);
+  } else {
+    return 'Number less than 0';
+  }
+}
+console.log(factorial(arg));
