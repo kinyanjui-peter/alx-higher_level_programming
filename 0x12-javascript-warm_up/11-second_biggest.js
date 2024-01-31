146d38aa9d25894efae7c8e9fb89319ec47ca958
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-31 18:10:26 +0300

    largest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* script that searches the second biggest integer in the list of arguments.
+ * script that searches the second biggest integer in the list of arguments.
+ * If no argument passed, print 0
+ * If the number of arguments is 1, print 0
+ * You must use console.log(...) to print all output
+ * You are not allowed to use var
+ */
+const process = require('process');
+const args = process.argv.slice(2);
+
+function largest_number(n) {
+  const length = n.length;
+
+  if (length <= 1) {
+    console.log(0);
+  } else {
+    const list = n.sort((a, b) => b - a);
+    const largest = list[1];
+    console.log(largest);
+  }
+}
+
+largest_number(args);
