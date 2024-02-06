8eb3ed55f1cd21c67f160521cacd8678d43f85fc
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-02-06 22:57:00 +0300

    finding peak number

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 # function that finds a peak in a list of unsorted integers
 
+
 def find_peak(list_of_integers):
     """ a function that findsthe largest number in alist"""
     # Initialize max_number to negative infinity
     max_number = float('-inf')
     numbers = list_of_integers
-
-
     if not list_of_integers:
-        ""' return none of the arguent is 0"""
+        """return none of the arguent is 0"""
         return None
 
     # Iterate through the list of numbers
